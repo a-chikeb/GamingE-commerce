c2c7b95eb880c1497e9a29bccfad3cbd794263d7
a-chikeb <aymen_chikeb@outlook.com>
2022-02-07 22:45:19 +0100

    Add some api function for product

--- src/main/java/com/programming/controllers/ProductController.java
+++ src/main/java/com/programming/controllers/ProductController.java
@@ -7,9 +7,12 @@ import com.programming.repositories.CategoryRepository;
 import com.programming.repositories.ColorRepository;
 import com.programming.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 @RestController
@@ -31,14 +34,39 @@ public class ProductController {
         return productRepository.findAll();
     }
 
-    @GetMapping("/category/{category}")
-    public List<Product> findProductByCategory(@PathVariable("category") String category){
-        return productRepository.findProductByCategory_Name(category);
+    @GetMapping("/category/{category}/{page}")
+    public List<Product> findProductByCategory(@PathVariable("category") String category,@PathVariable("page") int page){
+
+        //int size = productRepository.findProductByCategory_Name(category).size();
+        //int from = page > 0 ? (page-1) * 20 : 0;
+        //int to = page * 20;
+
+        //System.out.println("Page "+page);
+        //System.out.println("Search Category From "+from+" to "+to);
+
+        Pageable paging = PageRequest.of(page - 1, 20);
+        return productRepository.findProductByCategory_Name(category,paging);
     }
 
-    @GetMapping("/search/{name}")
-    public List<Product> searchByNameOrLetter(@PathVariable("name") String name){
-        return productRepository.findProductByNameContaining(name);
+
+
+    //  category/Moniteur-Et-Ecran-Gamer/12
+    @GetMapping("/category/{category}/size/{size}")
+    public List<Product> getProductByCategoryAndSize(@PathVariable("category") String category,@PathVariable("size") int size){
+
+        category = category.replace("-"," ").toLowerCase(Locale.ROOT);
+
+        Pageable paging = PageRequest.of(0, size);
+
+        return productRepository.findProductByCategory_Name(category,paging);
+    }
+
+
+
+    @GetMapping("/search/{name}/{page}")
+    public List<Product> searchByNameOrLetter(@PathVariable("name") String name, @PathVariable("page") int page){
+        Pageable paging = PageRequest.of(page - 1, 20);
+        return productRepository.findProductByNameContaining(name,paging);
     }
 
     @GetMapping("/{id}")
@@ -46,11 +74,11 @@ public class ProductController {
         return productRepository.findProductById(id);
     }
 
+
     @PostMapping("/add")
     public void addProduct(@RequestBody Map<String,Object> data){
 
-        /*Product product =
-        new Product(data.get("name").toString(),data.get("description").toString(),data.get("images").toString(),data.get("brand_images").toString(),Integer.parseInt(data.get("total").toString()),data.get("overview").toString());
+        /*Product product = new Product(data.get("name").toString(),data.get("description").toString(),data.get("images").toString(),data.get("brand_images").toString(),Integer.parseInt(data.get("total").toString()),data.get("overview").toString());
 
         Category cat = categoryRepository.findByName(data.get("category").toString());
         product.setCategory(cat);
--- src/main/java/com/programming/repositories/ProductRepository.java
+++ src/main/java/com/programming/repositories/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.programming.repositories;
 
 import com.programming.models.Product;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +9,8 @@ import java.util.*;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
+    List<Product> findProductByCategory_Name(String category, Pageable pageable);
     List<Product> findProductByCategory_Name(String category);
-    List<Product> findProductByNameContaining(String name);
+    List<Product> findProductByNameContaining(String name, Pageable pageable);
     Product findProductById(Long id);
 }
